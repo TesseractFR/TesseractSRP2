f4c43988e849c9bc67cc1289caac843f4bc84046
Gabriel Rayzal <gabriel.rayzal@gmail.com>
2025-03-03 18:11:47 +0100

    init

---
+++ src/main/kotlin/onl/tesseract/srp/TesseractSRP.kt
@@ -0,0 +1,14 @@
+package onl.tesseract.srp
+
+import org.bukkit.plugin.java.JavaPlugin
+
+class TesseractSRP : JavaPlugin() {
+
+    override fun onEnable() {
+        // Plugin startup logic
+    }
+
+    override fun onDisable() {
+        // Plugin shutdown logic
+    }
+}
