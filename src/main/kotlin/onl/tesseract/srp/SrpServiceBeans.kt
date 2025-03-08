143ceb8457828b3a96608ddb1125700df8738648
Gabriel Rayzal <gabriel.rayzal@gmail.com>
2025-03-08 22:07:09 +0100

    custom items (feat): set item id in persistent data container

---
+++ src/main/kotlin/onl/tesseract/srp/SrpServiceBeans.kt
@@ -0,0 +1,15 @@
+package onl.tesseract.srp
+
+import onl.tesseract.lib.persistantcontainer.NamedspacedKeyProvider
+import onl.tesseract.lib.service.ServiceContainer
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+
+@Configuration
+open class SrpServiceBeans {
+
+    @Bean
+    open fun namedSpacedKeyProvider(): NamedspacedKeyProvider {
+        return ServiceContainer[NamedspacedKeyProvider::class.java]
+    }
+}
