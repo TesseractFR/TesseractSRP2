f3cefbf469a7d8fa704a27f13388c4aa125c8b85
Gabriel Rayzal <gabriel.rayzal@gmail.com>
2025-03-08 22:07:09 +0100

    custom items (feat): set item id in persistent data container

--- src/main/kotlin/onl/tesseract/srp/service/item/CustomItemService.kt
+++ src/main/kotlin/onl/tesseract/srp/service/item/CustomItemService.kt
@@ -2,16 +2,18 @@ package onl.tesseract.srp.service.item
 
 import net.kyori.adventure.text.format.NamedTextColor
 import onl.tesseract.lib.menu.ItemBuilder
+import onl.tesseract.lib.persistantcontainer.NamedspacedKeyProvider
 import onl.tesseract.lib.util.plus
 import onl.tesseract.srp.domain.item.CustomMaterial
 import org.bukkit.inventory.ItemStack
+import org.bukkit.persistence.PersistentDataType
 import org.springframework.stereotype.Service
 
 @Service
-class CustomItemService {
+class CustomItemService(private val namespacedKeyProvider: NamedspacedKeyProvider) {
 
     fun createCustomItem(material: CustomMaterial, amount: Int = 1): ItemStack {
-        return ItemBuilder(material.baseMaterial)
+        val item = ItemBuilder(material.baseMaterial)
             .name(material.displayName)
             .color(NamedTextColor.GREEN)
             .lore()
@@ -20,5 +22,9 @@ class CustomItemService {
             .buildLore()
             .amount(amount)
             .build()
+        item.editMeta {
+            it.persistentDataContainer.set(namespacedKeyProvider.get("customMaterial"), PersistentDataType.STRING, material.name)
+        }
+        return item
     }
 }
