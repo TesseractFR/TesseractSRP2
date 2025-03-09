555c64d6ef154fb2d6ea804f9cce5b8d7351dc42
Gabriel Rayzal <gabriel.rayzal@gmail.com>
2025-03-09 10:48:53 +0100

    build (fix): use a compound class loader to expose bukkit resources to spring

---
+++ src/main/kotlin/onl/tesseract/srp/CompoundClassLoader.kt
@@ -0,0 +1,58 @@
+package onl.tesseract.srp
+
+import java.io.IOException
+import java.io.InputStream
+import java.net.URL
+import java.util.*
+
+class CompoundClassLoader(private val loaders: Collection<ClassLoader>) : ClassLoader() {
+
+    override fun getResource(name: String): URL? {
+        for (loader in loaders) {
+            loader.getResource(name)?.let { return it }
+        }
+        return null
+    }
+
+    override fun getResourceAsStream(name: String): InputStream? {
+        for (loader in loaders) {
+            loader.getResourceAsStream(name)?.let { return it }
+        }
+        return null
+    }
+
+    override fun getResources(name: String?): Enumeration<URL> {
+        val urls: MutableList<URL> = mutableListOf()
+
+        for (loader in this.loaders) {
+            try {
+                val resources = loader.getResources(name)
+
+                while (resources.hasMoreElements()) {
+                    val resource = resources.nextElement()
+                    if (resource != null && !urls.contains(resource)) {
+                        urls.add(resource)
+                    }
+                }
+            } catch (_: IOException) {
+            }
+        }
+
+        return Collections.enumeration(urls)
+    }
+
+    override fun loadClass(name: String): Class<*> {
+        for (loader in loaders) {
+            try {
+                return loader.loadClass(name)
+            } catch (_: ClassNotFoundException) {
+
+            }
+        }
+        throw ClassNotFoundException(name)
+    }
+
+    override fun loadClass(name: String, resolve: Boolean): Class<*> {
+        return this.loadClass(name)
+    }
+}
