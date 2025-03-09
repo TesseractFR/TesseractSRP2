262ac2451c3856b9e04a3758296674f8e2931e73
GuiguiShow <g.eckenschwiller@gmail.com>
2025-03-09 19:12:55 +0100

    Jobs,enum, BaseStat, QualityDistribution (config)

---
+++ src/main/kotlin/onl/tesseract/srp/domain/jobs/BaseStat.kt
@@ -0,0 +1,8 @@
+package onl.tesseract.srp.domain.jobs
+
+class BaseStat(
+    val lootChance: Float,
+    val moneyGain: Int,
+    val xpGain: Int,
+    val qualityDistribution: QualityDistribution
+)
