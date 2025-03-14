b0b419fb563ec9b11cbade97786652ae25534ce0
GuiguiShow <g.eckenschwiller@gmail.com>
2025-03-14 11:38:03 +0100

    Ajout sealed interface + CustomMaterial/BlockSource

--- src/main/kotlin/onl/tesseract/srp/domain/item/CustomMaterial.kt
+++ src/main/kotlin/onl/tesseract/srp/domain/item/CustomMaterial.kt
@@ -1,16 +1,23 @@
 package onl.tesseract.srp.domain.item
 
 import org.bukkit.Material
+import org.bukkit.entity.EntityType
+
+sealed interface CustomMaterialSource
+
+data class CustomMaterialBlockSource(val material: Material) : CustomMaterialSource
+
+data class CustomMaterialEntitySource(val entityType: EntityType) : CustomMaterialSource
 
 enum class CustomMaterial(
-    val dropSource : Any,
+    val dropSource: CustomMaterialSource,
     val customMaterial: Material,
     val displayName: String,
     val rarity: Rarity,
 ) {
-    Wood(Material.OAK_LOG, Material.OAK_LOG, displayName = "Bois", rarity = Rarity.Common),
-    BirchWood(Material.BIRCH_LOG, Material.BIRCH_LOG, displayName = "Bois de bouleau", Rarity.Common),
+    Wood(CustomMaterialBlockSource(Material.OAK_LOG), Material.OAK_LOG, "Bois", Rarity.Common),
+    BirchWood(CustomMaterialBlockSource(Material.BIRCH_LOG), Material.BIRCH_LOG, "Bois de bouleau", Rarity.Common),
 
-    ZombieDrop(org.bukkit.entity.EntityType.ZOMBIE, Material.ROTTEN_FLESH, "Chair Custom", Rarity.Rare),
-    SkeletonDrop(org.bukkit.entity.EntityType.SKELETON, Material.BONE, "Os Custom", Rarity.Rare)
+    ZombieDrop(CustomMaterialEntitySource(EntityType.ZOMBIE), Material.ROTTEN_FLESH, "Chair CUstom", Rarity.Rare),
+    SkeletonDrop(CustomMaterialEntitySource(EntityType.SKELETON), Material.BONE, "Os Custom", Rarity.Rare)
 }
