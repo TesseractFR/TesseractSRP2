12d6d43cead1c19cfac40056a84e10dfa96006d8
GuiguiShow <g.eckenschwiller@gmail.com>
2025-03-16 14:27:46 +0100

    rebase

--- src/main/kotlin/onl/tesseract/srp/controller/command/campement/CampementCommands.kt
+++ src/main/kotlin/onl/tesseract/srp/controller/command/campement/CampementCommands.kt
@@ -1,12 +1,14 @@
-package onl.tesseract.srp.controller.command.staff
+package onl.tesseract.srp.controller.command.campement
 
 import onl.tesseract.commandBuilder.CommandContext
 import onl.tesseract.commandBuilder.annotation.Command
 import onl.tesseract.commandBuilder.annotation.Perm
 import onl.tesseract.srp.SrpCommandInstanceProvider
 import onl.tesseract.srp.controller.command.campement.CreateCampCommand
+import onl.tesseract.srp.controller.command.campement.DeleteCampCommand
 
 @Command(name = "campement", subCommands = [
     CreateCampCommand::class,
+    DeleteCampCommand::class,
 ])
 class CampementCommands(commandInstanceProvider: SrpCommandInstanceProvider) : CommandContext(commandInstanceProvider) {}
--- src/main/kotlin/onl/tesseract/srp/repository/hibernate/CampementEntity.kt
+++ src/main/kotlin/onl/tesseract/srp/repository/hibernate/CampementEntity.kt
@@ -5,31 +5,38 @@ import onl.tesseract.srp.domain.campement.Campement
 import java.util.*
 
 @Entity
-@Table(name = "t_campements")
+@Table(name = "t_campements", indexes = [
+    Index(name = "idx_campement_chunks", columnList = "chunks")
+])
 class CampementEntity(
     @Id
-    val id: UUID,
-
-    @Column(nullable = false)
     val ownerID: UUID,
 
     @ElementCollection
+    @CollectionTable(name = "t_campements_trusted_players", joinColumns = [JoinColumn(name = "ownerID")])
+    @Column(name = "trusted_player")
     val trustedPlayers: List<UUID>,
 
     @Column(nullable = false)
     val chunks: Int,
 
     @ElementCollection
+    @CollectionTable(
+        name = "t_campements_chunks",
+        joinColumns = [JoinColumn(name = "ownerID")],
+        indexes = [Index(name = "idx_campement_chunk", columnList = "chunk")]
+    )
+    @Column(name = "chunk")
     val listChunks: List<String>,
 
     @Column(nullable = false)
     val campLevel: Int
 ) {
     fun toDomain(): Campement {
-        return Campement(id, ownerID, trustedPlayers, chunks, listChunks, campLevel)
+        return Campement(ownerID, ownerID, trustedPlayers, chunks, listChunks, campLevel)
     }
 }
 
 fun Campement.toEntity(): CampementEntity {
-    return CampementEntity(id, ownerID, trustedPlayers, chunks, listChunks, campLevel)
+    return CampementEntity(ownerID, trustedPlayers, chunks, listChunks, campLevel)
 }
