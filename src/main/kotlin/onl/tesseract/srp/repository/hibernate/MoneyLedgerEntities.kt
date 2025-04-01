3b0980e1f34ef5be6ee6b57dc0bf1081231982f1
Gabriel Rayzal <gabriel.rayzal@gmail.com>
2025-04-01 20:53:49 +0200

    money tracking (feat): table indexes

--- src/main/kotlin/onl/tesseract/srp/repository/hibernate/MoneyLedgerEntities.kt
+++ src/main/kotlin/onl/tesseract/srp/repository/hibernate/MoneyLedgerEntities.kt
@@ -20,12 +20,18 @@ class MoneyLedgerEntity(
 fun Ledger.toEntity(): MoneyLedgerEntity = MoneyLedgerEntity(id, type)
 
 @Entity
-@Table(name = "t_money_transaction")
+@Table(name = "t_money_transaction", indexes = [
+    Index(name = "ledger_from_idx", columnList = "ledger_from", unique = false),
+    Index(name = "ledger_to_idx", columnList = "ledger_to", unique = false),
+    Index(name = "date_idx", columnList = "date", unique = false),
+])
 class MoneyTransactionEntity(
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     var id: Long? = null,
+    @Column(name = "ledger_from")
     val ledgerFrom: UUID,
+    @Column(name = "ledger_to")
     val ledgerTo: UUID,
     @Enumerated(EnumType.STRING)
     val type: TransactionType,
