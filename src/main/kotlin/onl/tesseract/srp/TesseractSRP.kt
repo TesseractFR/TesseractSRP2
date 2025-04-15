929f6006a4d21a5abd5dab463c83d1b2c260d666
GuiguiShow <g.eckenschwiller@gmail.com>
2025-04-15 20:01:01 +0200

    elytra : creating elytras invocable

--- src/main/kotlin/onl/tesseract/srp/TesseractSRP.kt
+++ src/main/kotlin/onl/tesseract/srp/TesseractSRP.kt
@@ -7,9 +7,14 @@ import onl.tesseract.lib.persistence.yaml.equipment.EquipmentYamlRepository
 import onl.tesseract.lib.persistence.yaml.equipment.InvocableGenericSerializer
 import onl.tesseract.lib.service.ServiceContainer
 import onl.tesseract.srp.controller.command.staff.SrpStaffCommand
+import onl.tesseract.srp.domain.campement.AnnexionStickInvocable
+import onl.tesseract.srp.domain.equipment.ElytraInvocable
 import onl.tesseract.srp.domain.player.PlayerRank
 import onl.tesseract.srp.domain.world.SrpWorld
 import onl.tesseract.srp.repository.yaml.equipment.SrpInvocableSerializer
+import onl.tesseract.srp.repository.yaml.equipment.AnnexionStickSerializer
+import onl.tesseract.srp.repository.yaml.equipment.ElytraInvocableSerializer
+import onl.tesseract.srp.service.campement.CampementService
 import onl.tesseract.srp.service.world.WorldService
 import org.bukkit.event.Listener
 import org.bukkit.plugin.java.JavaPlugin
