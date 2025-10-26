cd56d78f5716e641a878444b6b0ea0f4dbf13098
aikiboo <florian.suchaud@hotmail.fr>
2025-10-26 19:38:23 +0100

    WIP création des menus custom

--- src/main/kotlin/onl/tesseract/srp/controller/event/structure/CustomStructureListener.kt
+++ src/main/kotlin/onl/tesseract/srp/controller/event/structure/CustomStructureListener.kt
@@ -4,6 +4,7 @@ import dev.lone.itemsadder.api.CustomFurniture
 import org.bukkit.entity.Player
 import org.bukkit.event.EventHandler
 import org.bukkit.event.Listener
+import org.bukkit.event.block.Action
 import org.bukkit.event.player.PlayerInteractEvent
 import org.bukkit.inventory.EquipmentSlot
 
@@ -15,7 +16,7 @@ abstract class CustomStructureListener() : Listener {
     fun onPlayerInteract(e: PlayerInteractEvent){
         val block = e.clickedBlock ?: return
         val furniture = CustomFurniture.byAlreadySpawned(block)?:return
-        if (e.hand != EquipmentSlot.HAND) return
+        if (e.hand != EquipmentSlot.HAND || e.action != Action.RIGHT_CLICK_BLOCK) return
         e.isCancelled = onClick(e.player,furniture)
 
     }
--- src/main/kotlin/onl/tesseract/srp/controller/event/structure/ForgeStructureListener.kt
+++ src/main/kotlin/onl/tesseract/srp/controller/event/structure/ForgeStructureListener.kt
@@ -1,18 +1,18 @@
 package onl.tesseract.srp.controller.event.structure
 
 import dev.lone.itemsadder.api.CustomFurniture
-import net.kyori.adventure.text.Component
-import onl.tesseract.srp.controller.menu.craftmanship.CraftingMenu
+import onl.tesseract.srp.controller.menu.skill.CraftingMenu
+import onl.tesseract.srp.service.item.CustomItemService
 import onl.tesseract.srp.service.skill.SkillService
 import org.bukkit.entity.Player
 import org.springframework.stereotype.Component as SpringComponent
 
 @SpringComponent
-class SkillStructureListener(val skillService: SkillService) :  CustomStructureListener() {
+class SkillStructureListener(val skillService: SkillService,val customItemService: CustomItemService) :  CustomStructureListener() {
     override fun onClick(player: Player, furniture: CustomFurniture) : Boolean{
         val skill = skillService.getSkillFromStructureID(furniture.namespacedID)?:return false
 
-        CraftingMenu(skill).open(player)
+        CraftingMenu(skill,customItemService).open(player)
 
         return true
     }
