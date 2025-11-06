5f49ba4f3f304bcbc7389d4187d6475bfcdda493
aikiboo <florian.suchaud@hotmail.fr>
2025-11-06 11:08:28 +0100

    WIP

--- src/main/kotlin/onl/tesseract/srp/service/skill/SkillService.kt
+++ src/main/kotlin/onl/tesseract/srp/service/skill/SkillService.kt
@@ -1,11 +1,14 @@
 package onl.tesseract.srp.service.skill
 
+import onl.tesseract.srp.domain.skill.CraftTask
 import onl.tesseract.srp.domain.skill.Skill
 import onl.tesseract.srp.repository.yaml.skill.SkillConfigRepository
 import org.springframework.stereotype.Component
+import java.util.UUID
 
 @Component
 class SkillService(val skillConfigRepository: SkillConfigRepository) {
+    private val activeTacks = mutableMapOf<UUID, Map<String, CraftTask>>()
 
     fun getSkillFromStructureID(structureID: String) : Skill?{
         return skillConfigRepository.getSkills()
