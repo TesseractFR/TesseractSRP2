88b2db94d59fca3b4093d0c199306f2244f0356d
GuiguiShow <g.eckenschwiller@gmail.com>
2026-05-01 16:53:42 +0200

    Front menu de craft

--- src/main/kotlin/onl/tesseract/srp/controller/menu/skill/CraftingMenu.kt
+++ src/main/kotlin/onl/tesseract/srp/controller/menu/skill/CraftingMenu.kt
@@ -12,8 +12,26 @@ import onl.tesseract.srp.domain.skill.Skill
 import onl.tesseract.srp.service.item.CustomItemService
 import org.bukkit.Material
 import org.bukkit.entity.Player
+import org.bukkit.inventory.ItemStack
 import kotlin.math.max
 import kotlin.math.min
+
+private const val COMPONENTS_OFFSET = -1
+private const val RESULT_INDEX = 8
+
+private const val INFO_BUTTON_INDEX = 18
+
+private const val QUANTITY_PLUS_FIRST_INDEX = 21
+private const val QUANTITY_PLUS_SECOND_INDEX = 22
+private const val QUANTITY_MAX_INDEX = 23
+
+private const val QUANTITY_DISPLAY_INDEX = 31
+private const val LAUNCH_BUTTON_INDEX = 34
+
+private const val RECIPE_BOOK_BUTTON_INDEX = 36
+private const val QUANTITY_MINUS_FIRST_INDEX = 39
+private const val QUANTITY_MINUS_SECOND_INDEX = 40
+private const val QUANTITY_MIN_INDEX = 41
 
 class CraftingMenu(val skill : Skill,
                    val customItemService: CustomItemService,
@@ -30,36 +48,46 @@ class CraftingMenu(val skill : Skill,
     override fun placeButtons(viewer: Player) {
         addRecipeButton(viewer)
         addActiveRecipe()
-
+        addInfoButton()
+        addLaunchButton()
         addQuantityButtons(viewer)
-
-        addBackButton()
-        addCloseButton()
     }
 
     private fun addQuantityButtons(viewer: Player) {
-        addButton(36, ItemBuilder(Material.PAPER,"min").build()){
+        addButton(QUANTITY_MIN_INDEX, customItemService.getCustomItem(CustomItemIds.MENU_MIN_BUTTON).also {
+            it.editMeta { m -> m.displayName(Component.text("min")) }
+        }) {
             quantityToCraft = 1
             this.placeButtons(viewer)
         }
-        addButton(37, ItemBuilder(Material.PAPER,"-$SECOND_OFFSET").build()){
-            quantityToCraft = max(1,quantityToCraft-SECOND_OFFSET)
+        addButton(QUANTITY_MINUS_SECOND_INDEX, customItemService.getCustomItem(CustomItemIds.MENU_MINUS_5_BUTTON).also {
+            it.editMeta { m -> m.displayName(Component.text("-$SECOND_OFFSET")) }
+        }) {
+            quantityToCraft = max(1, quantityToCraft - SECOND_OFFSET)
             this.placeButtons(viewer)
         }
-        addButton(38, ItemBuilder(Material.PAPER,"-$FIRST_OFFSET").build()){
-            quantityToCraft = max(1,quantityToCraft-FIRST_OFFSET)
+        addButton(QUANTITY_MINUS_FIRST_INDEX, customItemService.getCustomItem(CustomItemIds.MENU_MINUS_1_BUTTON).also {
+            it.editMeta { m -> m.displayName(Component.text("-$FIRST_OFFSET")) }
+        }) {
+            quantityToCraft = max(1, quantityToCraft - FIRST_OFFSET)
             this.placeButtons(viewer)
         }
-        addButton(39,ItemBuilder(Material.PAPER,"$quantityToCraft").build().asQuantity(quantityToCraft))
-        addButton(40, ItemBuilder(Material.PAPER,"+$FIRST_OFFSET").build()){
-            quantityToCraft = min(getMaxCraft(viewer),quantityToCraft+FIRST_OFFSET)
+        addButton(QUANTITY_DISPLAY_INDEX, ItemBuilder(Material.PAPER, "$quantityToCraft").build().asQuantity(quantityToCraft))
+        addButton(QUANTITY_PLUS_FIRST_INDEX, customItemService.getCustomItem(CustomItemIds.MENU_PLUS_1_BUTTON).also {
+            it.editMeta { m -> m.displayName(Component.text("+$FIRST_OFFSET")) }
+        }) {
+            quantityToCraft = min(getMaxCraft(viewer), quantityToCraft + FIRST_OFFSET)
             this.placeButtons(viewer)
         }
-        addButton(41, ItemBuilder(Material.PAPER,"+$SECOND_OFFSET").build()){
-            quantityToCraft = min(getMaxCraft(viewer),quantityToCraft+SECOND_OFFSET)
+        addButton(QUANTITY_PLUS_SECOND_INDEX, customItemService.getCustomItem(CustomItemIds.MENU_PLUS_5_BUTTON).also {
+            it.editMeta { m -> m.displayName(Component.text("+$SECOND_OFFSET")) }
+        }) {
+            quantityToCraft = min(getMaxCraft(viewer), quantityToCraft + SECOND_OFFSET)
             this.placeButtons(viewer)
         }
-        addButton(42, ItemBuilder(Material.PAPER,"max").build()){
+        addButton(QUANTITY_MAX_INDEX, customItemService.getCustomItem(CustomItemIds.MENU_MAX_BUTTON).also {
+            it.editMeta { m -> m.displayName(Component.text("max")) }
+        }) {
             quantityToCraft = getMaxCraft(viewer)
             this.placeButtons(viewer)
         }
@@ -67,17 +95,29 @@ class CraftingMenu(val skill : Skill,
 
     private fun addActiveRecipe() {
         activeRecipe.components.forEach { (i, component) ->
-            addButton(17+i,component.item.asQuantity(component.quantity*quantityToCraft))
+            addButton(COMPONENTS_OFFSET+i,component.item.asQuantity(component.quantity*quantityToCraft))
         }
-        addButton(26,activeRecipe.result.item.asQuantity(activeRecipe.result.quantity*quantityToCraft))
+        addButton(RESULT_INDEX,activeRecipe.result.item.asQuantity(activeRecipe.result.quantity*quantityToCraft))
+    }
+
+    private fun addInfoButton() {
+        val item = customItemService.getCustomItem(CustomItemIds.MENU_INFORMATION_BUTTON)
+        item.editMeta { it.displayName(Component.text("Informations")) }
+        addButton(INFO_BUTTON_INDEX, item) {}
     }
 
     private fun addRecipeButton(viewer: Player) {
         val item = customItemService.getCustomItem(CustomItemIds.MENU_RECIPE_BOOK_BUTTON)
         item.editMeta { it.displayName(Component.text("Recettes")) }
-        addButton(0, item) {
+        addButton(RECIPE_BOOK_BUTTON_INDEX, item) {
             RecipeMenu(skill, customItemService,playerInventoryPort, this).open(viewer);
         }
+    }
+
+    private fun addLaunchButton() {
+        val item = ItemStack(Material.FURNACE)
+        item.editMeta { it.displayName(Component.text("Lancer la fabrication")) }
+        addButton(LAUNCH_BUTTON_INDEX, item) {}
     }
 
     private fun getMaxCraft(viewer: Player): Int {
